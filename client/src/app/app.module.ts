26a857a46617d05032b28e20831aee1ec90d13bb
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-01-25 13:34:46 +1300

    Added a basic navigation framework.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
