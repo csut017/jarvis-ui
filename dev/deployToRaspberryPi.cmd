f47ffdd48920bc9db1bc97b1827ddc1bc226f628
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-02-02 17:57:01 +1300

    Deployment for first "real" testbed.

---
+++ dev/deployToRaspberryPi.cmd
@@ -0,0 +1,16 @@
+@ECHO off
+SETLOCAL ENABLEEXTENSIONS
+SET me=%~n0
+
+SET target=192.168.0.7
+
+ECHO %me%: Deploying to Raspberry Pi
+pscp -pw letmein1 ../server/server pi@%target%:/home/pi/arduino/quicktest
+pscp -pw letmein1 ./monitor.service pi@%target%:/home/pi/arduino/
+pscp -pw letmein1 ../server/config-pi.json pi@%target%:/home/pi/arduino/config.json
+REM pscp -pw letmein1 web/css/*.* pi@%target%:/home/pi/arduino/web/css/
+REM pscp -pw letmein1 web/html/*.* pi@%target%:/home/pi/arduino/web/html/
+REM pscp -pw letmein1 web/js/*.* pi@%target%:/home/pi/arduino/web/js/
+REM pscp -pw letmein1 web/media/*.* pi@%target%:/home/pi/arduino/web/media/
+
+ECHO %me%: Deploy completed
---
+++ remote/Plant/Plant.ino
@@ -0,0 +1,201 @@
+#include "DHT.h"
+
+// Input
+#define DHT_IN 2       // The DHT pin to use
+#define DHT_TYPE DHT11 // The type of DHT sensor
+#define LIGHT_IN A0    // The pin to read the soil moisture on
+#define SOIL_IN A2     // The pin to read the soil moisture on
+
+// Output
+#define NUMBER_OF_PINS 1
+#define LED_PIN 10   // The LED pin
+#define SOIL_POWER 8 // The pin to turn on the soil sensor
+#define PUMP_1_PIN 9 // The pump pin
+
+// Read times (in seconds)
+#define DHT_READ 2
+#define SOIL_READ 900
+int pinOnTimes[] = {0};
+
+// Internal variables
+DHT dht(DHT_IN, DHT_TYPE);    // Allows reading the DHT11 sensor
+bool ledOn = false;           // Whether the LED is on or not
+int loopCount = 0;            // The current loop count (always increases)
+String incomingCommand = "";  // The incoming command
+bool commandComplete = false; // Whether the command is complete and ready to be processed
+int pins[] = {PUMP_1_PIN};    // The associated pump pins
+bool pinOn = false;           // Whether to check any of the pins
+int photoresistor = 0;        // The last reading from the photoresistor
+int soilValue = 0;            // The last reading from the soil sensor
+int soilTime = 0;             // The cycles remaining until we read the soil sensor again
+
+void setup()
+{
+  dht.begin();
+  incomingCommand.reserve(200);
+
+  // Tell the monitor what we are exposing
+  Serial.begin(9600);
+  sendDetails();
+
+  // Initialise the pins
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  pinMode(PUMP_1_PIN, OUTPUT);
+  digitalWrite(PUMP_1_PIN, HIGH);
+  pinMode(SOIL_POWER, OUTPUT);
+  digitalWrite(SOIL_POWER, LOW);
+}
+
+void loop()
+{
+  // Flash the LED on a 1s on/1s off freq
+  delay(1000);
+  ledOn = !ledOn;
+  if (ledOn)
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  loopCount++;
+
+  if (pinOn)
+  {
+    checkPinTimes();
+  }
+
+  if (commandComplete)
+  {
+    if (incomingCommand[0] == 'C')
+    {
+      Serial.println(incomingCommand);
+      int pinToChange = (int)incomingCommand[2] - 48;
+      bool setPinOn = incomingCommand[3] == '+';
+      if (incomingCommand.length() > 4)
+      {
+        pinOnTimes[pinToChange] = incomingCommand.substring(4).toInt();
+      }
+      Serial.print("A:");
+      Serial.print(pinToChange);
+      if (setPinOn)
+      {
+        digitalWrite(pins[pinToChange], LOW);
+        Serial.println("+");
+      }
+      else
+      {
+        digitalWrite(pins[pinToChange], HIGH);
+        Serial.println("-");
+      }
+      pinOn = true;
+    } else if (incomingCommand[0] = 'I') {
+      sendDetails();
+    }
+
+    // Clear the last command so we can receive another one
+    incomingCommand = "";
+    commandComplete = false;
+  }
+
+  if (soilTime <= 0)
+  {
+    Serial.println("R:soil");
+    soilTime = SOIL_READ;
+    readSoil();
+  }
+  else
+  {
+    soilTime--;
+  }
+
+  // Only read at specified intervals
+  if (loopCount % DHT_READ == 0)
+  {
+    unsigned long timeSinceStart = millis();
+    float humidity = dht.readHumidity();
+    float temp = dht.readTemperature();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp))
+    {
+      Serial.println("E:DHT");
+      loopCount--; // Need to decrement otherwise it will wait the full five seconds
+      return;
+    }
+
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(temp, humidity, false);
+
+    // Read the light level
+    photoresistor = analogRead(LIGHT_IN);
+
+    // Send the data to the monitor
+    Serial.print("D:");
+    Serial.print(timeSinceStart);
+    Serial.print(",");
+    Serial.print(humidity);
+    Serial.print(",");
+    Serial.print(temp);
+    Serial.print(",");
+    Serial.print(hic);
+    Serial.print(",");
+    Serial.print(photoresistor);
+    Serial.print(",");
+    Serial.print(soilValue);
+    Serial.println();
+  }
+}
+
+void sendDetails()
+{
+  Serial.println("O:time,humidity,tempC,heatIndC,light,soil");
+  Serial.println("I:pump");
+}
+
+void checkPinTimes()
+{
+  int i;
+  pinOn = false;
+  for (i = 0; i < NUMBER_OF_PINS; i++)
+  {
+    if (pinOnTimes[i] > 0)
+    {
+      pinOn = true;
+      pinOnTimes[i]--;
+    }
+    else if (pinOnTimes[i] == 0)
+    {
+      digitalWrite(pins[i], HIGH);
+      pinOnTimes[i]--;
+      Serial.print("A:");
+      Serial.print(i);
+      Serial.println("-");
+    }
+  }
+}
+
+void serialEvent()
+{
+  while (Serial.available() && !commandComplete)
+  {
+    char inChar = (char)Serial.read();
+    incomingCommand += inChar;
+    // If the incoming character is a newline, set a flag so the main loop can process it
+    if (inChar == '\n')
+    {
+      commandComplete = true;
+    }
+  }
+}
+
+int readSoil()
+{
+  digitalWrite(SOIL_POWER, HIGH);
+  delay(10);
+  soilValue = analogRead(SOIL_IN);
+  digitalWrite(SOIL_POWER, LOW);
+}
